e6339c5827efc96f2b6ea3f82904d8a8becfdbfb
drb23 <drb23@student.le.ac.uk>
2021-12-22 17:23:56 +0000

    Added more comments to the piece controller explaining how the program collects each page of pieces from the Rebirckable API

--- Program/LegoSetCheckList/src/main/java/lego/checklist/controller/PieceController.java
+++ Program/LegoSetCheckList/src/main/java/lego/checklist/controller/PieceController.java
@@ -38,57 +38,25 @@ public class PieceController {
 		// This is the uri to the specific pieces in a set in the Rebrickable API
 		String piece_list_uri = rebrickable_uri + "sets/" + set_number + "/parts/?key=" + rebrickable_api_key;
 		
-//		// The rest template created above is used to fetch the Lego set every time the website is loaded
-//		// and here it uses the Lego set uri to call the API and then transforms the returned JSON into a String
-//		String piece_list_JSON = restTemplate.getForObject(piece_list_uri, String.class);
-		
 		// This creates an array list to store all the Lego pieces needed to build a Lego set
-		// This is declared here in case the following try catch statement fails
+		// This is declared here in case the try catch statement, in the getPiece_ListPage Class, fails
 		List<Piece> pieces =  new ArrayList<>();
 		
+		// This calls the getPiece_ListPage class that gets all the pieces in the Lego Set
 		pieces = getPiece_ListPage(piece_list_uri, pieces, restTemplate);
-		
-//		// This is wrapped in a try catch in case the string given to readTree() is not a JSON string
-//        try {
-//        	// This provides functionality for reading and writing JSON
-//        	ObjectMapper mapper = new ObjectMapper();
-//			
-//        	String next = null;
-//        	
-//        	// This provides the root node of the JSON string as a Tree and stores it in the class JsonNode
-//        	JsonNode piece_listNode = mapper.readTree(piece_list_JSON);
-//        	
-//        	JsonNode nextNode = piece_listNode.path("next");
-//        	next = nextNode.textValue();
-//			
-//        	// This provides the root of the JSON element where the JSON array of Lego pieces is stored
-//        	piece_listNode = piece_listNode.path("results");
-//        	
-//        	pieces = getPiece_ListPage(piece_listNode, pieces);
-//        	
-//    		
-//            if (next != null) {
-//            	piece_list_JSON = next + "&key=" + rebrickable_api_key;
-//            }
-//
-//		}
-//        catch (JsonMappingException e) {
-//			e.printStackTrace();
-//		}
-//        catch (JsonProcessingException e) {
-//			e.printStackTrace();
-//		}
     	
+		// This adds all the pieces in the Lego Set into the piece list class 
     	Piece_list piece_list = new Piece_list(pieces);
     	
     	model.addAttribute("piece_list", piece_list);
 		return "showPiece_list";
 	}
 	
-//	@GetMapping
+	// This gets all the pieces in the Lego Set using the Lego Set pieces uri, starting with the first page of these Lego piece list,
+	// If there are other pages containg pieces on the api, this class will then be called recursively to get all of these pieces
 	private List<Piece> getPiece_ListPage(String piece_list_uri, List<Piece> pieces, RestTemplate restTemplate) {
 		// The rest template created above is used to fetch the Lego set every time the website is loaded
-		// and here it uses the Lego set uri to call the API and then transforms the returned JSON into a String
+		// and here it uses the Lego set piece uri to call the API and then transforms the returned JSON into a String
 		String piece_list_JSON = restTemplate.getForObject(piece_list_uri, String.class);
 		
 		// This is wrapped in a try catch in case the string given to readTree() is not a JSON string
@@ -143,6 +111,8 @@ public class PieceController {
             	}
         	}
     		
+            // If their is another page of pieces that needed to be collected from the api here this page will be called recursively using this class
+            // and the piece list will be sent each time so when it is then returned it will contain all these pieces
             if (next != null) {
             	System.out.println("if");
             	piece_list_uri = next + "&key=" + rebrickable_api_key;
