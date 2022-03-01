1b7f9b24862470259392ba65cb8058a1ed6d04ff
drb23 <drb23@student.le.ac.uk>
2022-03-01 14:59:33 +0000

    Added JPA for database connection to program

---
+++ Program/LegoSetCheckList/src/main/java/lego/checklist/domain/Account.java
@@ -0,0 +1,27 @@
+package lego.checklist.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name= "Accounts")
+public class Account {
+	@Id
+	private String email;
+	
+	private String password;
+
+	public Account(String email, String password) {
+		this.email = email;
+		this.password = password;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+}
--- Program/LegoSetCheckList/src/main/java/lego/checklist/domain/Set_list.java
+++ Program/LegoSetCheckList/src/main/java/lego/checklist/domain/Set_list.java
@@ -1,20 +1,47 @@
 package lego.checklist.domain;
 
-public class Set_list {
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.persistence.Transient;
 
-	private String name;
+@Entity
+@Table(name= "SetLists")
+public class Set_list {
+	@Id
+	@GeneratedValue
+	private int setListId;
+	
+	@ManyToOne
+	@JoinColumn(name = "email", referencedColumnName = "email")
+	private Account account;
+	
+	private String listName;
+	
+	@Transient
 	private Set[] sets;
 	
-	public String getName() {
-		return name;
+	public int getSetListId() {
+		return setListId;
+	}
+
+	public Account getAccount() {
+		return account;
+	}
+
+	public String getListName() {
+		return listName;
 	}
 	
 	public Set[] getSets() {
 		return sets;
 	}
-	
-	public void setName(String name) {
-		this.name = name;
+
+	public void setListName(String listName) {
+		this.listName = listName;
 	}
 	
 	public void addList() {}
