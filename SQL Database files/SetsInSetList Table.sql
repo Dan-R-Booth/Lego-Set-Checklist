07a7807ca471ab8712657abd3f6a2e2a4097b361
drb23 <drb23@student.le.ac.uk>
2022-03-02 15:36:14 +0000

    Made small changes to sql table files

--- SQL Database files/SetsInSetList Table.sql
+++ SQL Database files/SetsInSetList Table.sql
@@ -1,9 +1,9 @@
 drop table if exists SetsInSetList;
 
 create table SetsInSetList (
-	setsInSetListId int not null,
+	setInSetListId int not null,
     setNumber varchar(20) not null,
     setListId int not null,
     foreign key (setListId) references SetLists (setListId),
-    primary key (setsInSetListId)
+    primary key (setInSetListId)
 );
